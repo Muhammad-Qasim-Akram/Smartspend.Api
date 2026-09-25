9df3f8940f85650e5ccdd40b2cc66588a8a670d3
Muhammad-Qasim-Akram <328572258+Muhammad-Qasim-Akram@users.noreply.github.com>
2026-09-25 23:39:48 +0500

    does the remaing model

--- Models/Budget.cs
+++ Models/Budget.cs
@@ -2,5 +2,11 @@ namespace Smartspend.Api.Models;
 
 public class Budget
 {
-    
+    public int Id {get; set;}
+    public required double Amount {get; set;}
+
+    public int  Day {get; set;}
+    public int  Month {get; set;}
+    public int  Year {get; set;}
+    public int UserId {get; set;}
 }
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -2,5 +2,10 @@ namespace Smartspend.Api.Models;
 
 public class Expense
 {
-    
+    public int Id {get; set;}
+    public double Amount {get; set;}
+    public string Description {get; set;}
+    public DateOnly Date {get; set;}
+    public int CategoryId {get; set;}
+    public int UserId {get; set;}
 }
