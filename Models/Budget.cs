588016f564b4aba6e70f7fc26c02aba564e5054c
Muhammad-Qasim-Akram <328572258+Muhammad-Qasim-Akram@users.noreply.github.com>
2026-09-26 06:20:04 +0500

    add fields for custom dates

--- Models/Budget.cs
+++ Models/Budget.cs
@@ -5,8 +5,8 @@ public class Budget
     public int Id {get; set;}
     public required decimal Amount {get; set;}
 
-    public int  Month {get; set;}
-    public int  Year {get; set;}
+    public DateOnly  StartDate {get; set;}
+    public DateOnly EndDate {get; set;}
     public int? UserId {get; set;}
     public int? CategoryId {get; set;}
     public Category? Category{get; set;}
