a557db82e5ccccde863c0828d9966fdeef2adf3a
Muhammad-Qasim-Akram <328572258+Muhammad-Qasim-Akram@users.noreply.github.com>
2026-09-26 06:20:53 +0500

    make lastname optional

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@ public class User
     public int Id {get; set;}
     public  required string Email {get; set;}
     public required string FirstName {get; set;}
-    public string LastName {get; set;}
+    public string? LastName {get; set;}
     public required string Password {get; set;}
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
 
